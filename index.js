ac6b8ceb28981818a9059364073a227853c202e6
Vivek Singh <vsingh.ggits.2010@gmail.com>
2018-12-22 13:19:53 +0530

    very first version of the app

---
+++ index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("searchq").addEventListener("keyup", function(){
+        getApiResponse();
+    });  
+});
+const URL = "https://raw.githubusercontent.com/viveksinghggits/acronym-db/master/db.json";
+
+function createXMLHttpRequestObject(){
+	
+	if(window.XMLHttpRequest){
+		xmlHTTPRequest = new XMLHttpRequest();
+	}
+	else{
+		xmlHTTPRequest = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return xmlHTTPRequest;
+}
+
+let _ = new createXMLHttpRequestObject();
+function getApiResponse(){
+    if(_!=null){
+        _.open("GET", URL, true);
+        _.onreadystatechange = processResponse;
+        _.send(null);
+    }
+    else{
+        console.log("Request object not created");
+    }
+    
+}
+
+function processResponse(){
+    let qs = document.getElementById("searchq").value;
+    if(_.readyState==4 && _.status==200){
+        
+        let jsObj = JSON.parse(_.responseText);
+        let wrapper = document.getElementById("fullform");
+        wrapper.innerHTML="";
+        for (var i=0; i<jsObj.length; i++){
+            if(jsObj[i].id.toUpperCase()==qs.toUpperCase()){// match if the ID is equal to the text that is there in the text box
+                let dEle = document.createElement("span")           
+                dEle.innerHTML=jsObj[i].value
+                wrapper.appendChild(dEle);
+            }
+        }
+    }
+}
